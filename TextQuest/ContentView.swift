61ef6ecb4e42be1953046cc088db308878e18e8f
Zachary Preator <zachary.preator@gmail.com>
2025-06-13 15:30:15 -0600

    Initial Commit

---
+++ TextQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextQuest
+//
+//  Created by Zachary Preator on 6/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextQuest/TextQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextQuestApp.swift
+//  TextQuest
+//
+//  Created by Zachary Preator on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TextQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
